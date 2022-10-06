c05438d76da80b163e80c595b4ee9eef8682b494
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-10-06 23:54:55 +0100

    Done

--- monty.h
+++ monty.h
@@ -12,14 +12,15 @@
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
+ *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-int n;
-struct stack_s *prev;
-struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 /**
  * struct bus_s - variables -args, file, line content
@@ -31,23 +32,24 @@ struct stack_s *next;
  */
 typedef struct bus_s
 {
-char *arg;
-FILE *file;
-char *content;
-int lifi;
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
 }  bus_t;
 extern bus_t bus;
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
+ *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
